531b43303117a55b336e04a2fdd17c5bc0bff517
Ramkumar Aiyengar <raiyengar@bloomberg.net>
2014-01-21 12:49:10 +0000

    SOLR-5620: Make ZkStateReader.aliases volatile, this can be updated async from a watcher

--- solr/solrj/src/java/org/apache/solr/common/cloud/ZkStateReader.java
+++ solr/solrj/src/java/org/apache/solr/common/cloud/ZkStateReader.java
@@ -135,7 +135,7 @@ public class ZkStateReader {
 
   private ZkCmdExecutor cmdExecutor;
 
-  private Aliases aliases = new Aliases();
+  private volatile Aliases aliases = new Aliases();
 
   private volatile boolean closed = false;
 
@@ -550,11 +550,13 @@ public class ZkStateReader {
   }
 
   public void updateAliases() throws KeeperException, InterruptedException {
-    byte[] data = zkClient.getData(ALIASES, null, null, true);
+    synchronized (getUpdateLock()) {
+      byte[] data = zkClient.getData(ALIASES, null, null, true);
 
-    Aliases aliases = ClusterState.load(data);
+      Aliases aliases = ClusterState.load(data);
 
-    ZkStateReader.this.aliases = aliases;
+      ZkStateReader.this.aliases = aliases;
+    }
   }
   
 }
