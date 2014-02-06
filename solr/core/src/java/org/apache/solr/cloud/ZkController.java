b3827645a313aa4d9e01a40cf1ae1fbcfe0a5aa7
Mark Robert Miller <markrmiller@apache.org>
2014-02-06 19:04:21 +0000

    SOLR-5702: Log config name found for collection at info level.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/branch_4x@1565400 13f79535-47bb-0310-9956-ffa450edef68

--- solr/core/src/java/org/apache/solr/cloud/ZkController.java
+++ solr/core/src/java/org/apache/solr/cloud/ZkController.java
@@ -733,16 +733,19 @@ public final class ZkController {
       configName = props.getStr(CONFIGNAME_PROP);
     }
     
-    if (configName != null && !zkClient.exists(CONFIGS_ZKNODE + "/" + configName, true)) {
-      log.error("Specified config does not exist in ZooKeeper:" + configName);
-      throw new ZooKeeperException(SolrException.ErrorCode.SERVER_ERROR,
-          "Specified config does not exist in ZooKeeper:" + configName);
+    if (configName != null) {
+      if (!zkClient.exists(CONFIGS_ZKNODE + "/" + configName, true)) {
+        log.error("Specified config does not exist in ZooKeeper:" + configName);
+        throw new ZooKeeperException(ErrorCode.SERVER_ERROR,
+            "Specified config does not exist in ZooKeeper:" + configName);
+      } else if (log.isInfoEnabled()) {
+        log.info("path={} {}={} specified config exists in ZooKeeper",
+            new Object[] {path, CONFIGNAME_PROP, configName});
+      }
     }
 
     return configName;
   }
-
-
 
   /**
    * Register shard with ZooKeeper.
