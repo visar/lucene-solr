b9eb5136577c3b40c72e34faa56cf7c6dbf22ad6
Christine Poerschke <cpoerschke@bloomberg.net>
2014-07-09 06:28:36 -0400

    Revert "solr: add admin/collections?action=DELETEREPLICA&core=... support"
    Summary: This reverts commit 4a491aca27763540688d0b593b5dcebdf8ec6a30.
    
    Test Plan: -
    
    Reviewers: raiyenga, dcollins, mjustice
    
    Reviewed By: raiyenga
    
    Differential Revision: https://all.phab.dev.bloomberg.com/D99186

--- solr/core/src/test/org/apache/solr/cloud/DeleteReplicaTest.java
+++ solr/core/src/test/org/apache/solr/cloud/DeleteReplicaTest.java
@@ -115,14 +115,7 @@ public class DeleteReplicaTest extends AbstractFullDistribZkTestBase {
       CloudSolrServer client, Replica replica, String shard)
       throws SolrServerException, IOException, InterruptedException {
     Map m = makeMap("collection", COLL_NAME, "action", DELETEREPLICA, "shard",
-        shard, "replica", replica.getName(), "core", replica.get("core"));
-    if (random().nextBoolean()) {
-      if (random().nextBoolean()) {
-        m.remove("replica");
-      } else {
-        m.remove("core");
-      }
-    }
+        shard, "replica", replica.getName());
     SolrParams params = new MapSolrParams(m);
     SolrRequest request = new QueryRequest(params);
     request.setPath("/admin/collections");
