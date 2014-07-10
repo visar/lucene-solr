4e212687f615a32d66478d3ef960602fccc2099f
Christine Poerschke <cpoerschke@bloomberg.net>
2014-07-10 10:39:06 -0400

    solr: distribTiming timing metrics to optionally include raw values
    Summary:
    D95618 added distribTiming breakdown itself.
    
    sample output before:
    
      "elapsed":{"count":4,"sum":599,"min":141,"max":161}
    
    sample output after:
    
      "elapsed":{"rawValues":[
    {"key":"http://nsrchdevnj2:15255/solr/collection1_shard4_replica1/","val":147},
    {"key":"http://nsrchdevnj1:15255/solr/collection1_shard3_replica1/","val":144},
    {"key":"http://nsrchdevnj2:15250/solr/collection1_shard2_replica1/","val":163},
    {"key":"http://nsrchdevnj1:15250/solr/collection1_shard1_replica1","val":213}],
    "count":4,"sum":667,"min":144,"max":213}
    
    Test Plan: -
    
    Reviewers: raiyenga
    
    Reviewed By: raiyenga
    
    Differential Revision: https://all.phab.dev.bloomberg.com/D99774

--- solr/core/src/java/org/apache/solr/handler/component/ResponseBuilder.java
+++ solr/core/src/java/org/apache/solr/handler/component/ResponseBuilder.java
@@ -63,7 +63,7 @@ public class ResponseBuilder
   private boolean needDocSet = false;
   private int fieldFlags = 0;
   //private boolean debug = false;
-  private boolean debugTimings, debugQuery, debugResults, debugTrack;
+  private boolean debugTimings, debugRawTimings, debugQuery, debugResults, debugTrack;
 
   private QParser qparser = null;
   private String queryString = null;
@@ -213,6 +213,7 @@ public class ResponseBuilder
   //-------------------------------------------------------------------------
 
   public boolean isDebug() {
+    // debugRawTimings deliberately not included
     return debugQuery || debugTimings || debugResults || debugTrack;
   }
 
@@ -221,12 +222,14 @@ public class ResponseBuilder
    * @return true if all debugging options are on
    */
   public boolean isDebugAll(){
+    // debugRawTimings deliberately not included
     return debugQuery && debugTimings && debugResults && debugTrack;
   }
 
   public void setDebug(boolean dbg){
     debugQuery = dbg;
     debugTimings = dbg;
+    // debugRawTimings deliberately left unchanged
     debugResults = dbg;
     debugTrack = dbg;
   }
@@ -245,6 +248,14 @@ public class ResponseBuilder
 
   public void setDebugTimings(boolean debugTimings) {
     this.debugTimings = debugTimings;
+  }
+
+  public boolean isDebugRawTimings() {
+    return debugRawTimings;
+  }
+
+  public void setDebugRawTimings(boolean debugRawTimings) {
+    this.debugRawTimings = debugRawTimings;
   }
 
   public boolean isDebugQuery() {
