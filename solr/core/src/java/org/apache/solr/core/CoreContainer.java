240619992925bd0d52c58b0bb09740ec719b3d4c
Christine Poerschke <cpoerschke@bloomberg.net>
2014-07-22 08:07:58 -0400

    solr: cores?action=UNLOAD can unregistered unclosed core
    Summary: Changing CoreAdminHandler to wait for core to close before unregistering it from ZK. Adding testMidUseUnload method to TestLazyCores.
    
    Test Plan: -
    
    Reviewers: raiyenga, dcollins, mjustice
    
    Reviewed By: raiyenga
    
    Differential Revision: https://all.phab.dev.bloomberg.com/D104260

--- solr/core/src/java/org/apache/solr/core/CoreContainer.java
+++ solr/core/src/java/org/apache/solr/core/CoreContainer.java
@@ -668,7 +668,7 @@ public class CoreContainer {
 
     core.unloadOnClose(deleteIndexDir, deleteDataDir, deleteInstanceDir);
     if (close)
-      core.close();
+      core.closeAndWait();
 
     if (zkSys.getZkController() != null) {
       try {
