88c3eff9d9673f773c91a74638b1faf0c35c1225
Ramkumar Aiyengar <raiyengar@bloomberg.net>
2015-01-02 18:47:03 +0000

    Fix typo bug in DistributedDebugComponentTest.testCompareWithNonDistributedRequest

--- solr/core/src/test/org/apache/solr/handler/component/DistributedDebugComponentTest.java
+++ solr/core/src/test/org/apache/solr/handler/component/DistributedDebugComponentTest.java
@@ -367,13 +367,14 @@ public class DistributedDebugComponentTest extends SolrJettyTestBase {
     assertEquals(distribResponse.getDebugMap().size() - 2, nonDistribResponse.getDebugMap().size());
 
     assertSectionEquals(distribResponse, nonDistribResponse, "explain");
+
     assertSectionEquals(distribResponse, nonDistribResponse, "rawquerystring");
     assertSectionEquals(distribResponse, nonDistribResponse, "querystring");
     assertSectionEquals(distribResponse, nonDistribResponse, "parsedquery");
     assertSectionEquals(distribResponse, nonDistribResponse, "parsedquery_toString");
     assertSectionEquals(distribResponse, nonDistribResponse, "QParser");
-    assertSectionEquals(distribResponse, nonDistribResponse, "filter_qieries");
-    assertSectionEquals(distribResponse, nonDistribResponse, "parsed_filter_qieries");
+    assertSectionEquals(distribResponse, nonDistribResponse, "filter_queries");
+    assertSectionEquals(distribResponse, nonDistribResponse, "parsed_filter_queries");
     
     // timing should have the same sections:
     assertSameKeys((NamedList<?>)nonDistribResponse.getDebugMap().get("timing"), (NamedList<?>)distribResponse.getDebugMap().get("timing"));
